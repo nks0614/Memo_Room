cf2044854240d6179dace0d82b69b0e8e660863c
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-06-26 22:15:10 +0900

    Activity code

--- app/src/main/java/study/kotlin/memo_room/AddActivity.kt
+++ app/src/main/java/study/kotlin/memo_room/AddActivity.kt
@@ -1,11 +1,40 @@
 package study.kotlin.memo_room
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add.*
+import study.kotlin.memo_room.DB.Memo
+import study.kotlin.memo_room.DB.MemoDB
 
 class AddActivity : AppCompatActivity() {
+    private var memoDB : MemoDB? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        memoDB = MemoDB.getInstance(this)
+
+        val addRunnable = Runnable {
+            val newMemo = Memo()
+            newMemo.title = edit_title.text.toString()
+            newMemo.content = edit_content.text.toString()
+            memoDB?.MemoDao()?.insert(newMemo)
+        }
+
+        add_text_btn.setOnClickListener {
+            val addThread = Thread(addRunnable)
+            addThread.start()
+
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        MemoDB.destroyInstance()
+        super.onDestroy()
     }
 }
--- app/src/main/java/study/kotlin/memo_room/MainActivity.kt
+++ app/src/main/java/study/kotlin/memo_room/MainActivity.kt
@@ -1,12 +1,55 @@
 package study.kotlin.memo_room
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import study.kotlin.memo_room.DB.Memo
+import study.kotlin.memo_room.DB.MemoDB
+import study.kotlin.memo_room.rcview.RcViewAdapter
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    
+    private var memoDB : MemoDB? = null
+    private var memoList = listOf<Memo>()
+    lateinit var mAdapter : RcViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        memoDB = MemoDB.getInstance(this)
+        mAdapter = RcViewAdapter(this, memoList)
+
+        val r = Runnable {
+            try {
+                memoList = memoDB?.MemoDao()?.getAll()!!
+                mAdapter = RcViewAdapter(this,memoList)
+                mAdapter.notifyDataSetChanged()
+
+                rcView.adapter = mAdapter
+                rcView.layoutManager = LinearLayoutManager(this)
+                rcView.setHasFixedSize(true)
+
+            } catch (e : Exception){
+                Log.d("checkk", "Error : $e")
+            }
+        }
+
+        val thread = Thread(r)
+        thread.start()
+
+        write_text_btn.setOnClickListener {
+            val i = Intent(this, AddActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+    override fun onDestroy() {
+        MemoDB.destroyInstance()
+        memoDB = null
+        super.onDestroy()
     }
 }
