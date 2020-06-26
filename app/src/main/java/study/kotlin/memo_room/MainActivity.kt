3dcb6d2db93a3f830ec1a04cceb44e2a78a6ea37
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-06-26 19:13:51 +0900

    START

---
+++ app/src/main/java/study/kotlin/memo_room/MainActivity.kt
@@ -0,0 +1,11 @@
+package study.kotlin.memo_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
