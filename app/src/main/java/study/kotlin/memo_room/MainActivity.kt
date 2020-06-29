76d4c81c0742c368dd212721ce50aabce933b057
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-06-29 10:20:15 +0900

    Revise Design

--- app/src/main/java/study/kotlin/memo_room/MainActivity.kt
+++ app/src/main/java/study/kotlin/memo_room/MainActivity.kt
@@ -1,9 +1,11 @@
 package study.kotlin.memo_room
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import study.kotlin.memo_room.DB.Memo
@@ -16,9 +18,13 @@ class MainActivity : AppCompatActivity() {
     private var memoList = listOf<Memo>()
     lateinit var mAdapter : RcViewAdapter
 
+    private lateinit var backPressHandler: OnBackPressHandler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        backPressHandler = OnBackPressHandler(this)
 
         memoDB = MemoDB.getInstance(this)
         mAdapter = RcViewAdapter(this, memoList)
@@ -51,5 +57,26 @@ class MainActivity : AppCompatActivity() {
         MemoDB.destroyInstance()
         memoDB = null
         super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        backPressHandler.onBackPressed()
+    }
+
+
+    inner class OnBackPressHandler(var activity : Activity){
+        private var backPressHandler: Long = 0
+
+        fun onBackPressed(){
+            if(System.currentTimeMillis() > backPressHandler+2000){
+                backPressHandler = System.currentTimeMillis()
+                Toast.makeText(this@MainActivity, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+            else{
+                finishAffinity()
+            }
+        }
+
     }
 }
